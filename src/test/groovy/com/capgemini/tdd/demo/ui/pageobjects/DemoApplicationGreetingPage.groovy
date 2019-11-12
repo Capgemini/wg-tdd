3887c28778d966ace2963286190166784a095ce8
tombrown <tombrown1002@gmail.com>
2019-11-12 10:01:27 +0000

    Added automated tests + minor change to index

---
+++ src/test/groovy/com/capgemini/tdd/demo/ui/pageobjects/DemoApplicationGreetingPage.groovy
@@ -0,0 +1,16 @@
+package com.capgemini.tdd.demo.ui.pageobjects
+
+import geb.Page
+
+class DemoApplicationGreetingPage extends Page {
+
+    static url = "http://localhost:8080"
+
+    static at = { browser.driver.currentUrl.contains(url) && greetingId.displayed }
+
+    static content = {
+        greetingId(required: true) { $("#greeting-id") }
+        greetingContent(required: true) { $("#greeting-content") }
+    }
+
+}
---
+++ src/test/groovy/com/capgemini/tdd/demo/ui/specs/DemoApplicationUISpec.groovy
@@ -0,0 +1,25 @@
+package com.capgemini.tdd.demo.ui.specs
+
+import com.capgemini.tdd.demo.ui.pageobjects.DemoApplicationGreetingPage
+import geb.spock.GebSpec
+
+class DemoApplicationUISpec extends GebSpec {
+
+    def "1 - The user accesses the Greeting page and asserts the page has loaded"() {
+        when: "The user navigates to the Greeting page"
+        to DemoApplicationGreetingPage
+
+        then: "The user asserts that the Greeting page has loaded"
+        at DemoApplicationGreetingPage
+    }
+
+    def "2 - The user accesses the Greeting page and asserts the content text is 'Hello, World!'"() {
+        when: "The user navigates to the Greeting page"
+        to DemoApplicationGreetingPage
+
+        then: "The user asserts that the Greeting page has loaded and the content text is 'Hello, World!'"
+        at DemoApplicationGreetingPage
+        assert greetingContent.text() == "Hello, World!"
+    }
+
+}
